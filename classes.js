d8db5a5f95cfad682e104352e20355d572a4b649
shubham-955 <shubhamgadge722@gmail.com>
2022-01-31 22:09:55 +0530

    For class eval

---
+++ classes.js
@@ -0,0 +1,28 @@
+class Rectangle {
+    constructor(width, length) {
+        this.width = width;
+        this.length = length;
+    }
+    getArea() {
+        return this.width * this.length;
+    }
+}
+
+class Sqaure extends Rectangle {
+    constructor(edge) {
+        super(edge, edge);
+        this.edge = edge;
+    }
+    getPerimeter() {
+        return 4 * this.edge;
+    }
+
+}
+var edge = 8;
+
+
+var square = new Sqaure(edge);
+
+
+console.log(square.getArea());
+console.log(square.getPerimeter());
